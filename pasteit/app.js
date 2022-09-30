68b53081239d617c36e8244e6db08efb68076381
cyberbuddy-manas <rspmanas1@gmail.com>
2022-09-30 20:17:27 +0530

    added firebase and done the id work

--- pasteit/app.js
+++ pasteit/app.js
@@ -1,5 +1,22 @@
 // Math.floor(Math.random()*(999-100+1)+100);
 
-let title =
+function sendData() {
+    let title = document.getElementById('title');
+    let paste = document.getElementById('paste');
+    let db = firebase.database();
+    let id;
 
-alert(title.type);
+    db.ref("id").once('value', (obj) => {
+        id = obj.val().id;
+    
+        db.ref("paste/" + id).set({
+            id: id,
+            title: title.value,
+            paste: paste.value
+        });
+
+        db.ref("id").set({
+            id: ++id
+        });
+    });
+}
